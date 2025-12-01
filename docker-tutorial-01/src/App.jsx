58d6614c5926c1501e3c5e46bb1dd50c21c11531
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-12-01 18:04:21 +0530

    Changed the line for testing v2

--- docker-tutorial-01/src/App.jsx
+++ docker-tutorial-01/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is a Docker Project</h1>
+      <h1>This is just a demo react project based on Docker Tutorial !!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
